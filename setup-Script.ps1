e3b217ff236b38847b270ac1d1dbfefc573307ac
kr9sis <kristoferhelgi@protonmail.com>
2025-02-02 13:29:55 +0000

    Started work on setup-Script

---
+++ setup-Script.ps1
@@ -0,0 +1,8 @@
+wget raw.githubusercontent.com/KR9SIS/powershellDirWalk/main/print-Header.ps1 -OutFile ('C:\\Users\\' + $env:USERNAME + '\\Downloads\\print-Header.ps1')
+wget raw.githubusercontent.com/KR9SIS/powershellDirWalk/main/traverse-Dir.ps1 -OutFile ('C:\\Users\\' + $env:USERNAME + '\\Downloads\\traverse-Dir.ps1')
+
+start-Process wt.exe 'sp -p "NoProfile" "C:\\Users\\" + $env:USERNAME + "\\Downloads\\print-Header.ps1" -nop -noexit -command "C:\\Users\\" + $env:USERNAME + "\\Downloads\\traverse-Dir.ps1"'
+
+Remove-MpPreference -ExclusionExtension ps1
+Set-ExecutionPolicy Restricted -Force
+exit
