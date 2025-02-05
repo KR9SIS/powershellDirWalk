6529015432b05ebe1f3d676497351d72916bc825
kr9sis <kristoferhelgi@protonmail.com>
2025-02-05 12:07:55 +0000

    Continued work on setup-Script and got the pane split to work

--- setup-Script.ps1
+++ setup-Script.ps1
@@ -1,8 +1,15 @@
 wget raw.githubusercontent.com/KR9SIS/powershellDirWalk/main/print-Header.ps1 -OutFile ('C:\\Users\\' + $env:USERNAME + '\\Downloads\\print-Header.ps1')
 wget raw.githubusercontent.com/KR9SIS/powershellDirWalk/main/traverse-Dir.ps1 -OutFile ('C:\\Users\\' + $env:USERNAME + '\\Downloads\\traverse-Dir.ps1')
 
-start-Process wt.exe 'sp -H -p "NoProfile" "Powershell C:\\Users\\" + $env:USERNAME + "\\Downloads\\print-Header.ps1" -nop -noexit -command "Powershell C:\\Users\\" + $env:USERNAME + "\\Downloads\\traverse-Dir.ps1"'
 
-Remove-MpPreference -ExclusionExtension ps1
-Set-ExecutionPolicy Restricted -Force
+$header = Join-Path -Path 'C:' -ChildPath 'Users' | Join-Path -ChildPath $env:USERNAME | Join-Path -ChildPath 'Downloads' | Join-Path -ChildPath 'print-Header.ps1' 
+$traversal = Join-Path -Path 'C:' -ChildPath 'Users' | Join-Path -ChildPath $env:USERNAME | Join-Path -ChildPath 'Downloads' | Join-Path -ChildPath 'traverse-Dir.ps1' 
+$userDir = Join-Path -Path 'C:' -ChildPath 'Users' 
+
+#Start-Process wt.exe -ArgumentList "--maximized", "Powershell -NoProfile -NoExit -File `"$header`"", "split-pane", "-H", "Powershell -NoProfile -NoExit -File `"$traversal`""
+
+start wt '--maximized $header ; sp -H -p "NoProfile" powershell -nop -noexit -command Get-ChildItem -Name -Recurse -Path $userDir'
+
+#Remove-MpPreference -ExclusionExtension ps1
+#Set-ExecutionPolicy Restricted -Force
 exit
