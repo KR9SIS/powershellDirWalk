b848b513f36bf5937c74769335bab50b612a8894
kr9sis <kristoferhelgi@protonmail.com>
2025-02-06 14:34:56 +0000

    Cleaned up script and deleted old less functional backup

--- setup-Script.ps1
+++ setup-Script.ps1
@@ -1,9 +1,9 @@
-wget raw.githubusercontent.com/KR9SIS/powershellDirWalk/main/print-Header.ps1 -OutFile ('C:\\Users\\' + $env:USERNAME + '\\Downloads\\print-Header.ps1')
-wget raw.githubusercontent.com/KR9SIS/powershellDirWalk/main/traverse-Dir.ps1 -OutFile ('C:\\Users\\' + $env:USERNAME + '\\Downloads\\traverse-Dir.ps1')
-
 $header = Join-Path -Path 'C:' -ChildPath 'Users' | Join-Path -ChildPath $env:USERNAME | Join-Path -ChildPath 'Downloads' | Join-Path -ChildPath 'print-Header.ps1' 
 $traversal = Join-Path -Path 'C:' -ChildPath 'Users' | Join-Path -ChildPath $env:USERNAME | Join-Path -ChildPath 'Downloads' | Join-Path -ChildPath 'traverse-Dir.ps1' 
 $userDir = Join-Path -Path 'C:' -ChildPath 'Users' 
+
+wget raw.githubusercontent.com/KR9SIS/powershellDirWalk/main/print-Header.ps1 -OutFile $header 
+wget raw.githubusercontent.com/KR9SIS/powershellDirWalk/main/traverse-Dir.ps1 -OutFile $traversal 
 
 Start-Process wt.exe -ArgumentList "--maximized", "Powershell -NoProfile -NoExit -File `"$header`"", ";", "split-pane", "-H", "Powershell -NoProfile -NoExit -File `"$traversal`""
 
