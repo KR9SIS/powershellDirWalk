73d42f5b3965cae25ba2676669903bcc721e4e43
kr9sis <kristoferhelgi@protonmail.com>
2025-02-06 11:26:41 +0000

    Updated traverse-Dir to print out first user files then system files

--- traverse-Dir.ps1
+++ traverse-Dir.ps1
@@ -1,8 +1,4 @@
-Write-Host ("`n`nYour files:")
+Get-ChildItem -Path C:\Users\$env:USERNAME\* -Exclude *AppData* -Name -Recurse -Force
 
-Get-ChildItem -Path C:\Users\$env:USERNAME\* -Exclude *AppData* -Name -Recurse -Force | ForEach-Object {
-    Write-Host -NoNewline "$($_)`r"
-    Start-Sleep -Milliseconds 25  # Optional delay to visualize the overwrite
-}
-
+Get-ChildItem -Path C:\* -Name -Recurse -Force
 
